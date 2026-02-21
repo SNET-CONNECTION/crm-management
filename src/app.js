598050ae3af25808d7bac978328879fc08b0a388
SNET-CONNECTION <snetkoneksi@gmail.com>
2026-02-21 21:11:19 +0700

    Fix static file serving middleware syntax

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.set('views', path.resolve(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/public', express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(session({
   secret: env.sessionSecret,

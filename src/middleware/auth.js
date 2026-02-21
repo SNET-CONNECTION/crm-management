a19c601ead32c2995731a030680b7ba55c381d55
SNET-CONNECTION <snetkoneksi@gmail.com>
2026-02-21 18:10:44 +0700

    Add authentication and role middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+function requireAuth(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  return next();
+}
+
+function requireRole(...roles) {
+  return (req, res, next) => {
+    if (!req.session.user) return res.redirect('/login');
+    if (!roles.includes(req.session.user.role)) {
+      return res.status(403).render('dashboard/forbidden', { user: req.session.user });
+    }
+    return next();
+  };
+}
+
+module.exports = { requireAuth, requireRole };
